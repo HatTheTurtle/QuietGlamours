6b300d8e4e6d2e6bb8b0f517d9ab0d5c7a7e6a32
Hattu <hattheturtle@gmail.com>
2021-05-04 03:25:29 -0400

    Initial commit
    Initial commit; EN, FR, DE, JP supported. Very basic GUI, probably remove in favour of chat command toggle

---
+++ QuietGlamours/QuietGlamours.cs
@@ -0,0 +1,82 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.Plugin;
+using Dalamud.Game.Text;
+using Dalamud.Game.Text.SeStringHandling;
+using Dalamud.Game.Text.SeStringHandling.Payloads;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace QuietGlamours
+{
+    public class Plugin : IDalamudPlugin
+    {
+        public string Name => "Quiet Glamours";
+        private const string commandName = "/qg";
+
+        private DalamudPluginInterface pi;
+        private Configuration configuration;
+        private QuietGlamoursUI ui;
+
+        public void Initialize(DalamudPluginInterface pluginInterface)
+        {
+            this.pi = pluginInterface;
+
+            this.configuration = this.pi.GetPluginConfig() as Configuration ?? new Configuration();
+            this.configuration.Initialize(this.pi);
+
+            this.ui = new QuietGlamoursUI(this.configuration);
+
+            this.pi.CommandManager.AddHandler(commandName, new CommandInfo(OnCommand)
+            {
+                HelpMessage = "Open config window for Quiet Glamours"
+            });
+
+            this.pi.UiBuilder.OnBuildUi += DrawUI;
+            this.pi.UiBuilder.OnOpenConfigUi += (sender, args) => DrawConfigUI();
+            this.pi.Framework.Gui.Chat.OnChatMessage += OnChatMessage;
+        }
+
+        public void Dispose()
+        {
+            this.ui.Dispose();
+            this.pi.CommandManager.RemoveHandler(commandName);
+            this.pi.Framework.Gui.Chat.OnChatMessage -= OnChatMessage;
+        }
+
+        private void OnCommand(string command, string args)
+        {
+            // in response to the slash command, just display our main ui
+            this.ui.SettingsVisible = true;
+        }
+
+        //EN: You cast a glamour.
+        //FR: Vous projetez un mirage.
+        //DE: Du projizierst ein
+        //JP: の外見を武具投影した。
+        private void OnChatMessage(XivChatType type, uint senderId, ref SeString sender, ref SeString message, ref bool isHandled)
+        {
+            if (this.configuration.enabled)
+            {
+                if ((message.TextValue.Contains("You cast a glamour.") | message.TextValue.Contains("の外見を武具投影した") | 
+                    message.TextValue.Contains("Vous projetez un mirage.") | message.TextValue.Contains("Du projizierst ein")) & 
+                    (type & XivChatType.SystemMessage) == XivChatType.SystemMessage)
+                {
+                    isHandled = true;
+                }
+            }
+        }
+
+        private void DrawUI()
+        {
+            this.ui.Draw();
+        }
+
+        private void DrawConfigUI()
+        {
+            this.ui.SettingsVisible = true;
+        }
+    }
+}
---
+++ QuietGlamours/QuietGlamoursUI.cs
@@ -0,0 +1,59 @@
+﻿using ImGuiNET;
+using System;
+using System.Numerics;
+
+namespace QuietGlamours
+{
+    // It is good to have this be disposable in general, in case you ever need it
+    // to do any cleanup
+    class QuietGlamoursUI : IDisposable
+    {
+        private Configuration configuration;
+
+        // this extra bool exists for ImGui, since you can't ref a property
+
+        private bool settingsVisible = false;
+        public bool SettingsVisible
+        {
+            get { return this.settingsVisible; }
+            set { this.settingsVisible = value; }
+        }
+
+        public QuietGlamoursUI(Configuration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Draw()
+        {
+            DrawSettingsWindow();
+        }
+
+        public void DrawSettingsWindow()
+        {
+            if (!SettingsVisible)
+            {
+                return;
+            }
+
+            ImGui.SetNextWindowSize(new Vector2(250, 65), ImGuiCond.Always);
+            if (ImGui.Begin("Quiet Glamours", ref this.settingsVisible,
+                ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse))
+            {
+                // can't ref a property, so use a local copy
+                var configValue = this.configuration.enabled;
+                if (ImGui.Checkbox("Suppress system glamour messages", ref configValue))
+                {
+                    this.configuration.enabled = configValue;
+                    // can save immediately on change, if you don't want to provide a "Save and Close" button
+                    this.configuration.Save();
+                }
+            }
+            ImGui.End();
+        }
+    }
+}
